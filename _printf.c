688db00446ee344ecf60f8337aa0c83d2fecbc13
kofimuad <iamkofiagyei@gmail.com>
2024-01-31 01:45:44 +0000

    edited printf.c

--- _printf.c
+++ _printf.c
@@ -7,20 +7,24 @@
 
 int _printf(const char *format, ...)
 {
-va_list ap;
-int charsprinted;
-charsprinted = 0;
+	if (format == NULL)
+		return (-1);
 
-va_start(ap, format);
+	va_list ap;
+	int charsprinted;
 
-while (*format != '\0')
-{
-if (*format == '%')
-charsprinted = charsprinted + _printSpecifier(*(++format), ap);
-else
-charsprinted = charsprinted + write(1, format, 1);
-format++;
-}
-va_end(ap);
-return (charsprinted);
+	charsprinted = 0;
+
+	va_start(ap, format);
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+			charsprinted = charsprinted + _printSpecifier(*(++format), ap);
+		else
+			charsprinted = charsprinted + write(1, format, 1);
+		format++;
+	}
+	va_end(ap);
+	return (charsprinted);
 }
